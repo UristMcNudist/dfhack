f6d04cfff8cb8b01dbf1d34212a9f2cdec5c8d98
UristMcNudist <grandmacobol@gmx.de>
2018-06-25 20:28:49 +0200

    updated seedwatch.cpp & plant list

--- plugins/seedwatch.cpp
+++ plugins/seedwatch.cpp
@@ -27,8 +27,131 @@ REQUIRE_GLOBAL(world);
 
 const int buffer = 20; // seed number buffer - 20 is reasonable
 
-// abbreviations for the standard plants
-map<string, string> abbreviations;
+// vectors for all farmable plant types
+const vector<string> plants_standard =
+{
+    "MUSHROOM_HELMET_PLUMP",
+    "GRASS_TAIL_PIG",
+    "GRASS_WHEAT_CAVE",
+    "POD_SWEET",
+    "BUSH_QUARRY",
+    "ROOT_MUCK",
+    "TUBER_BLOATED",
+    "BULB_KOBOLD",
+    "BERRIES_PRICKLE",
+    "BERRIES_STRAW",
+    "GRASS_LONGLAND",
+    "HERB_VALLEY",
+    "WEED_RAT",
+    "BERRIES_FISHER",
+    "REED_ROPE",
+    "MUSHROOM_CUP_DIMPLE",
+    "WEED_BLADE",
+    "ROOT_HIDE",
+    "SLIVER_BARB",
+    "BERRY_SUN",
+    "VINE_WHIP"
+};
+
+const vector<string> plants_crops =
+{
+    "SINGLE-GRAIN_WHEAT",
+    "TWO-GRAIN_WHEAT",
+    "SOFT_WHEAT",
+    "HARD_WHEAT",
+    "SPELT",
+    "BARLEY",
+    "BUCKWHEAT",
+    "OATS",
+    "ALFALFA",
+    "RYE",
+    "SORGHUM",
+    "RICE",
+    "MAIZE",
+    "QUINOA",
+    "KANIWA",
+    "BITTER_VETCH",
+    "PENDANT_AMARANTH",
+    "BLOOD_AMARANTH",
+    "PURPLE_AMARANTH",
+    "RED_SPINACH",
+    "ELEPHANT-HEAD_AMARANTH",
+    "PEARL_MILLET",
+    "WHITE_MILLET",
+    "FINGER_MILLET",
+    "FOXTAIL_MILLET",
+    "FONIO",
+    "TEFF",
+    "FLAX",
+    "JUTE",
+    "HEMP",
+    "COTTON",
+    "RAMIE",
+    "KENAF",
+    "PAPYRUS_SEDGE"
+};
+
+const vector<string> plants_garden =
+{
+    "ARTICHOKE",
+    "ASPARAGUS",
+    "BAMBARA_GROUNDNUT",
+    "STRING_BEAN",
+    "BROAD_BEAN",
+    "BEET",
+    "BITTER_MELON",
+    "CABBAGE",
+    "CAPER",
+    "WILD_CARROT",
+    "CASSAVA",
+    "CELERY",
+    "CHICKPEA",
+    "CHICORY",
+    "COWPEA",
+    "CUCUMBER",
+    "EGGPLANT",
+    "GARDEN_CRESS",
+    "GARLIC",
+    "HORNED_MELON",
+    "LEEK",
+    "LENTIL",
+    "LETTUCE",
+    "MUNG_BEAN",
+    "MUSKMELON",
+    "ONION",
+    "PARSNIP",
+    "PEA",
+    "PEANUT",
+    "PEPPER",
+    "POTATO",
+    "RADISH",
+    "RED_BEAN",
+    "RHUBARB",
+    "SOYBEAN",
+    "SPINACH",
+    "SQUASH",
+    "SWEET_POTATO",
+    "TARO",
+    "TOMATO",
+    "TOMATILLO",
+    "TURNIP",
+    "URAD_BEAN",
+    "WATERMELON",
+    "WINTER_MELON",
+    "LESSER_YAM",
+    "LONG_YAM",
+    "PURPLE_YAM",
+    "WHITE_YAM",
+    "PASSION_FRUIT",
+    "GRAPE",
+    "CRANBERRY",
+    "BILBERRY",
+    "BLUEBERRY",
+    "BLACKBERRY",
+    "RASPBERRY",
+    "PINEAPPLE"
+};
+
 
 bool ignoreSeeds(df::item_flags& f) // seeds with the following flags should not be counted
 {
@@ -59,44 +182,24 @@ void printHelp(color_ostream &out) // prints help
         );
     out.print(
         "Options:\n"
-        "seedwatch all   - Adds all plants from the abbreviation list to the watch list.\n"
-        "seedwatch start - Start watching.\n"
-        "seedwatch stop  - Stop watching.\n"
-        "seedwatch info  - Display whether seedwatch is watching, and the watch list.\n"
-        "seedwatch clear - Clears the watch list.\n\n"
+        "seedwatch all N      - Adds all plants with limit N to the watch list.\n"
+        "seedwatch standard N - Adds standard plants with limit N to the watch list.\n"
+        "seedwatch crops N    - Adds crop plants with limit N to the watch list.\n"
+        "seedwatch garden N   - Adds garden plants with limit N to the watch list.\n"
+        "seedwatch start      - Start watching.\n"
+        "seedwatch stop       - Stop watching.\n"
+        "seedwatch info       - Display whether seedwatch is watching, and the watch list.\n"
+        "seedwatch clear      - Clears the watch list.\n\n"
         );
-    if(!abbreviations.empty())
-    {
-        out.print("You can use these abbreviations for the plant tokens:\n");
-        for(map<string, string>::const_iterator i = abbreviations.begin(); i != abbreviations.end(); ++i)
-        {
-            out.print("%s -> %s\n", i->first.c_str(), i->second.c_str());
-        }
-    }
     out.print(
         "Examples:\n"
         "seedwatch MUSHROOM_HELMET_PLUMP 30\n"
         "  add MUSHROOM_HELMET_PLUMP to the watch list, limit = 30\n"
         "seedwatch MUSHROOM_HELMET_PLUMP\n"
         "  removes MUSHROOM_HELMET_PLUMP from the watch list.\n"
-        "seedwatch ph 30\n"
-        "  is the same as 'seedwatch MUSHROOM_HELMET_PLUMP 30'\n"
         "seedwatch all 30\n"
         "  adds all plants from the abbreviation list to the watch list, the limit being 30.\n"
         );
-};
-
-// searches abbreviations, returns expansion if so, returns original if not
-string searchAbbreviations(string in)
-{
-    if(abbreviations.count(in) > 0)
-    {
-        return abbreviations[in];
-    }
-    else
-    {
-        return in;
-    }
 };
 
 DFhackCExport command_result plugin_enable(color_ostream &out, bool enable)
@@ -128,6 +231,13 @@ command_result df_seedwatch(color_ostream &out, vector<string>& parameters)
 
     string par;
     int limit;
+    auto setLimit = [&materialsReverser, &limit](vector<string> in)
+    {
+        for (vector<string>::const_iterator i = in.begin(); i != in.end(); ++i)
+        {
+            if (materialsReverser.count(*i) > 0) Kitchen::setLimit(materialsReverser[*i], limit);
+        }
+    };
     switch(parameters.size())
     {
     case 0:
@@ -207,7 +317,7 @@ command_result df_seedwatch(color_ostream &out, vector<string>& parameters)
         */
         else
         {
-            string token = searchAbbreviations(par);
+            string token = par;
             if(materialsReverser.count(token) > 0)
             {
                 Kitchen::removeLimit(materialsReverser[token]);
@@ -222,16 +332,21 @@ command_result df_seedwatch(color_ostream &out, vector<string>& parameters)
     case 2:
         limit = atoi(parameters[1].c_str());
         if(limit < 0) limit = 0;
-        if(parameters[0] == "all")
+        if (parameters[0] == "standard" || parameters[0] == "all")
         {
-            for(map<string, string>::const_iterator i = abbreviations.begin(); i != abbreviations.end(); ++i)
-            {
-                if(materialsReverser.count(i->second) > 0) Kitchen::setLimit(materialsReverser[i->second], limit);
-            }
+            setLimit(plants_standard);
+        }
+        else if (parameters[0] == "crops" || parameters[0] == "all")
+        {
+            setLimit(plants_crops);
+        }
+        else if (parameters[0] == "garden" || parameters[0] == "all")
+        {
+            setLimit(plants_garden);
         }
         else
         {
-            string token = searchAbbreviations(parameters[0]);
+            string token = parameters[0];
             if(materialsReverser.count(token) > 0)
             {
                 Kitchen::setLimit(materialsReverser[token], limit);
@@ -255,28 +370,6 @@ command_result df_seedwatch(color_ostream &out, vector<string>& parameters)
 DFhackCExport command_result plugin_init(color_ostream &out, vector<PluginCommand>& commands)
 {
     commands.push_back(PluginCommand("seedwatch", "Toggles seed cooking based on quantity available", df_seedwatch));
-    // fill in the abbreviations map, with abbreviations for the standard plants
-    abbreviations["bs"] = "SLIVER_BARB";
-    abbreviations["bt"] = "TUBER_BLOATED";
-    abbreviations["bw"] = "WEED_BLADE";
-    abbreviations["cw"] = "GRASS_WHEAT_CAVE";
-    abbreviations["dc"] = "MUSHROOM_CUP_DIMPLE";
-    abbreviations["fb"] = "BERRIES_FISHER";
-    abbreviations["hr"] = "ROOT_HIDE";
-    abbreviations["kb"] = "BULB_KOBOLD";
-    abbreviations["lg"] = "GRASS_LONGLAND";
-    abbreviations["mr"] = "ROOT_MUCK";
-    abbreviations["pb"] = "BERRIES_PRICKLE";
-    abbreviations["ph"] = "MUSHROOM_HELMET_PLUMP";
-    abbreviations["pt"] = "GRASS_TAIL_PIG";
-    abbreviations["qb"] = "BUSH_QUARRY";
-    abbreviations["rr"] = "REED_ROPE";
-    abbreviations["rw"] = "WEED_RAT";
-    abbreviations["sb"] = "BERRY_SUN";
-    abbreviations["sp"] = "POD_SWEET";
-    abbreviations["vh"] = "HERB_VALLEY";
-    abbreviations["ws"] = "BERRIES_STRAW_WILD";
-    abbreviations["wv"] = "VINE_WHIP";
     return CR_OK;
 }
 
